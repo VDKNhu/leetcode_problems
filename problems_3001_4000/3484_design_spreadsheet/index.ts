f0c898bab5f163b6f1d35353442ef6cda09a8cbc
Kim Nhu <nhuvdk@gmail.com>
2025-09-19 15:07:22 +0000

    problem 3484

---
+++ problems_3001_4000/3484_design_spreadsheet/index.ts
@@ -0,0 +1,29 @@
+class Spreadsheet {
+    spsh: Map<string, number>;
+
+    constructor(rows: number) {
+        this.spsh = new Map<string, number>();
+    }
+
+    setCell(cell: string, value: number): void {
+        this.spsh.set(cell, value);
+    }
+
+    resetCell(cell: string): void {
+        this.spsh.delete(cell);
+    }
+
+    getValue(formula: string): number {
+        let res = 0;
+        const terms = formula.slice(1).split("+");
+        for(const term of terms) {
+            const numbericValue = Number(term);
+            if(isNaN(numbericValue)) {
+                res += this.spsh.get(term) || 0;
+            } else {
+                res += numbericValue;
+            }
+        }
+        return res;
+    }
+}
