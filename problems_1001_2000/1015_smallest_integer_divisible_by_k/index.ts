483cf420bdc1cbff8dd92ca4625c35bb1e784083
VDKNhu <nhuvdk@gmail.com>
2025-11-25 23:10:02 +0700

    problem 1015 - add explaination

--- problems_1001_2000/1015_smallest_integer_divisible_by_k/index.ts
+++ problems_1001_2000/1015_smallest_integer_divisible_by_k/index.ts
@@ -1,3 +1,9 @@
+// Pigeonhole Principle
+// There are only k possible remainders (0 through k-1). 
+// If we haven't found a 0 remainder after k iterations, 
+// we'll start seeing repeated remainders, meaning we're in a cycle that doesn't include 0, 
+// so no repunit will ever be divisible by k.
+
 function smallestRepunitDivByK(k: number): number {
     if(k % 2 === 0 || k % 5 === 0) return -1;
 
