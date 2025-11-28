44b7ad2f6396b31b1a7191a1c12ffe721baaaf14
VDKNhu <nhuvdk@gmail.com>
2025-11-28 22:25:22 +0700

    problem 1437

--- problems_1001_2000/1437_check_if_all_1s_are_at_least_length_k_places_away/index.ts
+++ problems_1001_2000/1437_check_if_all_1s_are_at_least_length_k_places_away/index.ts
@@ -13,3 +13,16 @@ function kLengthApart(nums: number[], k: number): boolean {
 
     return true;
 };
+
+function kLengthApartV2(nums: number[], k: number): boolean {
+    let minDistance = Number.MAX_SAFE_INTEGER;
+    let prevIndex = -1;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === 1 && prevIndex !== -1) {
+            minDistance = Math.min(minDistance, i - prevIndex - 1);
+        } 
+        if(nums[i] === 1) prevIndex = i;
+    }
+
+    return minDistance >= k;
+};
